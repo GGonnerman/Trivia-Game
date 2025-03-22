f74969f0d0e24a30472b2c929999bb33091bb44c
GGonnerman <gaston95g@gmail.com>
2025-03-22 13:28:18 -0500

    Updated schema and made docker compose for setting up MySQL

--- trivia-database-generation/mysql-files/trivia_schema_creation.sql
+++ trivia-database-generation/mysql-files/trivia_schema_creation.sql
@@ -5,16 +5,16 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema trivia
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `mydb` ;
+DROP SCHEMA IF EXISTS `trivia` ;
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema trivia
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `trivia` DEFAULT CHARACTER SET utf8mb4 ;
 SHOW WARNINGS;
-USE `mydb` ;
+USE `trivia` ;
 
 -- -----------------------------------------------------
 -- Table `User`
